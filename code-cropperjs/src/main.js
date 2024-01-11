ad1dba03ab04795646cff3c0cc3ff92dd95f6f3d
mongkeywh <arrogancewh@163.com>
2024-01-11 18:52:55 +0800

    提交代码

--- code-cropperjs/src/main.js
+++ code-cropperjs/src/main.js
@@ -1,5 +1,24 @@
 import Cropper from 'cropperjs'
 
 console.dir(Cropper)
-const imgDom = document.querySelector('img')
-new Cropper(imgDom)
+const imgDom1 = document.querySelector('#img1')
+const imgDom2 = document.querySelector('#img2')
+const btn = document.querySelector('button')
+const cropper = new Cropper(
+  imgDom1,
+)
+cropper.getCroppedCanvas().toBlob((blob) => {
+  console.log(blob)
+})
+
+btn.addEventListener('click', () => {
+  cropper.getCroppedCanvas().toBlob((blob => {
+    console.log(blob)
+    const reader = new FileReader()
+    reader.addEventListener('load', () => {
+      console.log(reader.result)
+      imgDom2.setAttribute('src', reader.result)
+    })
+    reader.readAsDataURL(blob)  
+  }))
+})
